3cf4267ad74a896400b977d2142d97070c0372d0
asmallbit <im.jianweihan@gmail.com>
2022-05-31 20:51:40 +0800

    Add the wind speed, humidity, uv index, pressure, visibility, dew point panel

--- app/src/main/java/ml/ruby/weatherrecyclerview/MainActivity.java
+++ app/src/main/java/ml/ruby/weatherrecyclerview/MainActivity.java
@@ -26,11 +26,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import ml.ruby.weatherrecyclerview.adapter.HourlyWeatherAdapter;
 import ml.ruby.weatherrecyclerview.databinding.ActivityMainBinding;
+import ml.ruby.weatherrecyclerview.model.onecall.Current;
 import ml.ruby.weatherrecyclerview.model.onecall.Hourly;
+import ml.ruby.weatherrecyclerview.model.onecall.OneCallBean;
+import ml.ruby.weatherrecyclerview.model.onecall.Weather;
 import ml.ruby.weatherrecyclerview.repository.PlacesRepository;
 import ml.ruby.weatherrecyclerview.repository.WeatherRepository;
+import ml.ruby.weatherrecyclerview.utils.Constants;
 import ml.ruby.weatherrecyclerview.utils.ExecutorSupplier;
+import ml.ruby.weatherrecyclerview.utils.NumberOperation;
 import ml.ruby.weatherrecyclerview.utils.PreferenceManage;
+import ml.ruby.weatherrecyclerview.utils.Utility;
 import ml.ruby.weatherrecyclerview.view.HourlyWeatherFragment;
 import ml.ruby.weatherrecyclerview.view.PlacesActivity;
 import ml.ruby.weatherrecyclerview.view.WeatherFragment;
@@ -57,47 +63,7 @@ public class MainActivity extends AppCompatActivity {
             binding.locationPlaceName.setText(preferenceManage.getString("cityName"));
         }
 
-        // ToastMessage
-        PlacesRepository.getInstance().getToastMessage().observe(this, toastMessage ->
-                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show()
-        );
-
-        // 天气数据拉取失败提示
-        WeatherRepository.getInstance().getIsSucceed().observe(this, isSucceed -> {
-            if (!isSucceed) {
-                Toast.makeText(MainActivity.this, "Connect to open weather. Maybe you don't have internet now.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // ProgressBar
-        WeatherRepository.getInstance().getIsLoaded().observe(this, this::isLoaded
-        );
-
-        // 更新位置
-        placesViewModel.updateLocation();
-
-        placesViewModel.getPlaceToSwitch().observe(this, placeBeanItem -> {
-                    // TODO: Chang the search action bar icon
-                    binding.locationPlaceName.setText(placeBeanItem.getAreaFullName());
-                }
-        );
-
-        // 检测GPS是否打开
-        placesViewModel.isGPSEnabled().observe(this, isGPSEnabled -> {
-            if (!isGPSEnabled) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Please open the gps")
-                        .setMessage("We need the gps to get your location. Please enable the gps")
-                        .setPositiveButton("Yes", (dialog, which) ->
-                                MainActivity.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)))
-                        .setNegativeButton("No", (dialog, which) ->
-                                Toast.makeText(MainActivity.this,
-                                        "Okay, we will show London's weather as we cant get your location", Toast.LENGTH_SHORT).show())
-                        .setCancelable(false)
-                        .show();
-            }
-        });
+        setObserver(placesViewModel);
 
         binding.searchView.setOnClickListener(v -> startActivity(new Intent(this, PlacesActivity.class)));
 
@@ -139,11 +105,76 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
+    private void setObserver(PlacesViewModel placesViewModel) {
+        // ToastMessage
+        PlacesRepository.getInstance().getToastMessage().observe(this, toastMessage ->
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show()
+        );
+
+        // 天气数据拉取失败提示
+        WeatherRepository.getInstance().getIsSucceed().observe(this, isSucceed -> {
+            if (!isSucceed) {
+                Toast.makeText(MainActivity.this, "Can't connect to open weather. Maybe you don't have internet now.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // ProgressBar
+        WeatherRepository.getInstance().getIsLoaded().observe(this, this::isLoaded
+        );
+
+        // 更新位置
+        placesViewModel.updateLocation();
+
+        placesViewModel.getPlaceToSwitch().observe(this, placeBeanItem -> {
+                    binding.locationPlaceName.setText(placeBeanItem.getAreaFullName());
+                    binding.progressbar.setVisibility(View.VISIBLE);
+                }
+        );
+
+        // 检测GPS是否打开
+        placesViewModel.isGPSEnabled().observe(this, isGPSEnabled -> {
+            if (!isGPSEnabled) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Please open the gps")
+                        .setMessage("We need the gps to get your location. Please enable the gps")
+                        .setPositiveButton("Yes", (dialog, which) ->
+                                MainActivity.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)))
+                        .setNegativeButton("No", (dialog, which) ->
+                                Toast.makeText(MainActivity.this,
+                                        "Okay, we will show London's weather as we cant get your location", Toast.LENGTH_SHORT).show())
+                        .setCancelable(false)
+                        .show();
+            }
+        });
+
+        // 监听天气是否刷新
+        WeatherRepository.getInstance().getWeatherInfo().observe(this, this::setWeatherWidget);
+    }
+
     private void isLoaded(boolean isLoaded) {
         if (isLoaded) {
             binding.progressbar.setVisibility(View.GONE);
         } else {
             binding.progressbar.setVisibility(View.VISIBLE);
         }
+    }
+
+    private void setWeatherWidget(OneCallBean bean) {
+        Current current = bean.getCurrent();
+        Weather currentWeather = bean.getCurrent().getWeather().get(0);
+        binding.weatherWidget.setBackgroundResource(R.color.weather_widget_background_color);
+        binding.weatherIcon.setImageResource(Utility.getWeatherArtImage(currentWeather.getId()));
+        binding.weatherInfo.setText(currentWeather.getDescription());
+        binding.temperature.setText((int) (current.getTemp() - Constants.KELVINS) + "℃");
+        binding.feelsLike.setText("Feels like " +
+                (int) (current.getFeelsLike() - Constants.KELVINS) + "℃");
+        binding.windSpeed.setText("Wind: " + NumberOperation.round(current.getWindSpeed(), 1) + "m/s "
+                + Utility.convertDegreeToCardinalDirection(current.getWindDeg()));
+        binding.humidity.setText("Humidity: " + current.getHumidity() + "%");
+        binding.uvIndex.setText("UV index: " + NumberOperation.round(current.getUvi(), 1));
+        binding.pressure.setText("Pressure: " + current.getPressure() + "hPa");
+        binding.visibility.setText("Visibility: " + current.getVisibility() / 1000 + "km");
+        binding.dewPoint.setText("Dew point: " + (int) (current.getDewPoint() - Constants.KELVINS) + "℃");
     }
 }
